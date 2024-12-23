a7d54c5b7533264ff87b987561cd0cdb91021639
tanmay-exists <tanmayrewale232021@gmail.com>
2024-12-23 17:34:29 +0530

    .

---
+++ nvim/lua/plugins/devicons.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "kyazdani42/nvim-web-devicons",
+    opts = {
+      override = {
+        JetBrainsMono = {
+          icon = "", -- Example icon, customize as needed
+          color = "#ff6c6b", -- Customize the color
+          name = "JetBrainsMono",
+        },
+      },
+    },
+  },
+}
